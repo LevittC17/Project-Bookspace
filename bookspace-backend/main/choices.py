73d26b67f749c5a4493b611779d6228e4b561465
Peter Evance <peterevance1@gmail.com>
2023-09-07 18:26:20 +0300

    One more choice...

--- bookspace-backend/main/choices.py
+++ bookspace-backend/main/choices.py
@@ -1,10 +1,12 @@
 from django.db import models
 
+
 class BookTagChoices(models.TextChoices):
-    FICTION= 'Fiction'
+    FICTION = 'Fiction'
     FANTASY = 'Fantasy'
     COMEDY = 'Comedy'
     ADVENTURE = 'Adventure'
     ROMANCE = 'Romance'
     SCIFI = 'Sci-Fi'
     HISTORY = 'History'
+    SELF_IMPROVEMENT = 'Self Improvement'
