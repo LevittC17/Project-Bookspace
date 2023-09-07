7b78e98ba02f62d7361ecf02ab6f32a91abd48aa
Peter Evance <peterevance1@gmail.com>
2023-09-07 18:24:54 +0300

    Added admin related code and urls...

--- bookspace-backend/main/admin.py
+++ bookspace-backend/main/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from main.models import Book
+from main.models import *
 # Register your models here.
 
 admin.site.register(Book)
+admin.site.register(BookTag)
+admin.site.register(Author)
+admin.site.register(BookImage)
+
